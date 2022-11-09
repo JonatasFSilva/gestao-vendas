891cc96b6781a01c48b27ab0d65ba89412d2acf5
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-11-09 12:29:29 -0300

    ^.

---
+++ src/main/java/com/gvendas/gestaovendas/config/SpringFoxCongif.java
@@ -0,0 +1,36 @@
+package com.gvendas.gestaovendas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxCongif {
+
+	@Bean
+	public Docket swagger() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.gvendas"))						
+				.paths(PathSelectors.any())				
+				.build()
+				.apiInfo(infoApi());
+	}
+
+	private ApiInfo infoApi() {
+		return new ApiInfoBuilder()
+				.title("Gestão de Vendas")
+				.description("Sistema de Gestão de Vendas")
+				.version("1.0.0")
+				.build();
+	}
+
+}
