83aa4d03f96565c2735dae61397c2cbc9d8efc17
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-11-09 12:07:40 -0300

    .

--- src/main/java/com/gvendas/gestaovendas/exceptions/GestaoVendasExceptionHandler.java
+++ src/main/java/com/gvendas/gestaovendas/exceptions/GestaoVendasExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.gvendas.gestaovendas.exceptions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +12,35 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GestaoVendasExceptionHandler extends ResponseEntityExceptionHandler {
 
+	private static final String CONSTANT_VALIDATION_LENGTH = "Length";
+	private static final String CONSTANT_VALIDATION_NOT_BLANK = "NotBlank";
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		gerarListErrors(ex.getBindingResult());
+		List<Error> errors = gerarListErrors(ex.getBindingResult());
 
-		return super.handleMethodArgumentNotValid(ex, headers, status, request);
+		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
 	}
 
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
+			WebRequest request) {
+		String msgUser = "Rescurso não encontrado.";
+		String msgDev = ex.toString();
+		List<Error> errors = Arrays.asList(new Error(msgUser, msgDev));
+		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+
+	}
+
+	// METODOS INTERNOS DE EXECUCAO E TRATAMENTOS
 	private List<Error> gerarListErrors(BindingResult bindingResult) {
 
 		List<Error> errors = new ArrayList<>();
@@ -32,14 +49,18 @@ public class GestaoVendasExceptionHandler extends ResponseEntityExceptionHandler
 			String msgDev = fieldError.toString();
 			errors.add(new Error(msgUser, msgDev));
 		});
-
 		return errors;
-
 	}
 
 	private String handleMessageUser(FieldError fieldError) {
-		if(fieldError.getCode().equals(fieldError))
-		return null;
+		if (fieldError.getCode().equals(CONSTANT_VALIDATION_NOT_BLANK)) {
+			return fieldError.getDefaultMessage().concat(" é obrigatório.");
+		}
+		if (fieldError.getCode().equals(CONSTANT_VALIDATION_LENGTH)) {
+			return fieldError.getDefaultMessage().concat(String.format(" deve ter entre %s e %s caracteres.",
+					fieldError.getArguments()[2], fieldError.getArguments()[1]));
+		}
+		return fieldError.toString();
 	}
 
 }
