641d7cb54b7c3ae59e51f72981a125be80f2be58
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-11-16 09:20:18 -0300

    .

--- src/main/java/com/gvendas/gestaovendas/controllers/ProdutoController.java
+++ src/main/java/com/gvendas/gestaovendas/controllers/ProdutoController.java
@@ -1,5 +1,40 @@
 package com.gvendas.gestaovendas.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gvendas.gestaovendas.entities.Produto;
+import com.gvendas.gestaovendas.services.ProdutoService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "Produto")
+@RestController
+@RequestMapping("/produto")
 public class ProdutoController {
+
+	@Autowired
+	ProdutoService produtoService;
+
+	@ApiOperation(value = "Lista todos os Produtos")
+	@GetMapping
+	public List<Produto> findAll() {
+		return produtoService.findAll();
+	}
+
+	@ApiOperation(value = "Lista o Produto pelo ID")
+	@GetMapping("{/id}")
+	public ResponseEntity<Optional<Produto>> findById(@PathVariable Long id) {
+		Optional<Produto> produto = produtoService.findById(id);
+		return produto.isPresent() ? ResponseEntity.ok(produto) : ResponseEntity.notFound().build();
+	}
 
 }
--- src/main/java/com/gvendas/gestaovendas/services/ProdutoService.java
+++ src/main/java/com/gvendas/gestaovendas/services/ProdutoService.java
@@ -1,5 +1,26 @@
 package com.gvendas.gestaovendas.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gvendas.gestaovendas.entities.Produto;
+import com.gvendas.gestaovendas.repositories.ProdutoRepository;
+
+@Service
 public class ProdutoService {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+
+	public List<Produto> findAll() {
+		return produtoRepository.findAll();
+	}
+
+	public Optional<Produto> findById(Long id) {
+		return produtoRepository.findById(id);
+	}
 
 }
