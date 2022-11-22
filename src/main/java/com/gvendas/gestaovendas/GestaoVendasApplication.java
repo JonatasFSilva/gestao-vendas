e706fc3b75dc57155ba7b981b17e0256fd630fb0
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-11-22 11:29:18 -0300

    .

--- src/main/java/com/gvendas/gestaovendas/GestaoVendasApplication.java
+++ src/main/java/com/gvendas/gestaovendas/GestaoVendasApplication.java
@@ -1,19 +1,17 @@
 package com.gvendas.gestaovendas;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class GestaoVendasApplication {
 
-	 @Bean
+	 /**
+	  * @Bean
 	    public HttpTraceRepository httpTraceRepository() {
 	        return new InMemoryHttpTraceRepository();
-	    }
+	    }**/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoVendasApplication.class, args);
