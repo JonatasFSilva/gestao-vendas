52c06c9708ee8d5ae02df15efc00ac66b2c04bf5
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-11-23 10:31:27 -0300

    .

---
+++ src/main/java/com/gvendas/gestaovendas/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.gvendas.gestaovendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gvendas.gestaovendas.entities.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
